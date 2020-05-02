bc122a3469ae278c52d86f20e022daa59bd7c8fa
Pratik Rami <pratikrami1998@outlook.com>
2020-05-02 21:45:18 +0530

    Basic Contact Form Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl, Validators} from '@angular/forms';
+interface Subject {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-
+  inputControl = new FormControl('', Validators.required);
+  subjects: Subject[] = [
+    {value: 'Metal Exterior Louvers', viewValue: 'Metal Exterior Louvers'},
+    {value: 'Metal False Ceilings', viewValue: 'Metal False Ceilings'},
+    {value: 'Wooden Texture', viewValue: 'Wooden Texture'}
+  ];
   constructor() { }
 
   ngOnInit() {
