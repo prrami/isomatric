3bea7fed89d5f13fe8967cad4c525b2bf60f31ff
Pratik Rami <pratikrami1998@outlook.com>
2020-07-27 23:35:34 +0530

    Home page Gallery added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+  constructor(private route: ActivatedRoute,
+    private httpClient: HttpClient) { }
 
   ngOnInit() {
-  }
-
+    this.galleryOptions = [
+      { "thumbnails": false ,"thumbnailsColumns": 8, "width":"100%", "height": "90vh", "imageAnimation": "zoom" , "imageArrowsAutoHide": true, "thumbnailsArrowsAutoHide": true,"imageAutoPlay": true, "imageAutoPlayPauseOnHover": true, "previewAutoPlay": true, "previewAutoPlayPauseOnHover": false },
+      { "breakpoint": 500, "width": "100%", "height": "50vh", "thumbnailsColumns": 3 },
+      { "breakpoint": 300, "width": "100%", "height": "50vh", "thumbnailsColumns": 2 },
+      ];
+    this.galleryImages = [
+      {
+        "big": "assets/Products/Metal False Ceilings/Z Shaped Screen/big 1.jpg",
+        "medium": "assets/Products/Metal False Ceilings/Z Shaped Screen/big 1.jpg",
+        "small": "assets/Products/Metal False Ceilings/Z Shaped Screen/small 1.jpg"
+      },
+      {
+        "big": "assets/Products/Metal False Ceilings/Z Shaped Screen/big 2.jpg",
+        "medium": "assets/Products/Metal False Ceilings/Z Shaped Screen/big 2.jpg",
+        "small": "assets/Products/Metal False Ceilings/Z Shaped Screen/small 2.jpg"
+      },
+      {
+        "big": "assets/Products/Metal False Ceilings/U Shaped Screen/big 1.jpg",
+        "medium": "assets/Products/Metal False Ceilings/U Shaped Screen/big 1.jpg",
+        "small": "assets/Products/Metal False Ceilings/U Shaped Screen/small 1.jpg"
+      },
+      {
+        "big": "assets/Products/Metal False Ceilings/U Shaped Screen/big 2.jpg",
+        "medium": "assets/Products/Metal False Ceilings/U Shaped Screen/big 2.jpg",
+        "small": "assets/Products/Metal False Ceilings/U Shaped Screen/small 2.jpg"
+      }
+    ];
+    }
 }
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -21,47 +21,14 @@ export class ProductDetailComponent implements OnInit{
      }
     
   ngOnInit() {
-    debugger;
     this.getProduct();
-    debugger;
     this.galleryOptions = [
       { "width":"100%", "height": "80vh", "imageAnimation": "zoom" , "imageArrowsAutoHide": true, "thumbnailsArrowsAutoHide": true,"imageAutoPlay": true, "imageAutoPlayPauseOnHover": true, "previewAutoPlay": true, "previewAutoPlayPauseOnHover": true },
       { "breakpoint": 500, "width": "100%", "height": "50vh", "thumbnailsColumns": 3 },
       { "breakpoint": 300, "width": "100%", "height": "50vh", "thumbnailsColumns": 2 },
       ];
-      debugger;
-  this.galleryImages = [];
-  
-
-  debugger;
-  // [
-  //     {
-  //       small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-small.jpeg',
-  //       medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-medium.jpeg',
-  //       big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-big.jpeg'
-  //     },
-  //     {
-  //       small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-small.jpeg',
-  //       medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-medium.jpeg',
-  //       big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-big.jpeg'
-  //     },
-  //     {
-  //       small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-small.jpeg',
-  //       medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-medium.jpeg',
-  //       big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-big.jpeg'
-  //     },
-  //     {
-  //       small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-small.jpeg',
-  //       medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-medium.jpeg',
-  //       big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-big.jpeg'
-  //     },
-  //     {
-  //       small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-small.jpeg',
-  //       medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-medium.jpeg',
-  //       big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-big.jpeg'
-  //     }      
-  // ];
-  }
+    this.galleryImages = [];
+    }
   
   
   getProduct(): void {
@@ -71,11 +38,9 @@ export class ProductDetailComponent implements OnInit{
     // console.log(this.subtitle);
     // const id = +this.route.snapshot.paramMap.get('id');
     this.httpClient.get("assets/ProductDetail.json").subscribe(data =>{
-        debugger;
         this.products = data;
         this.product = this.products.filter(x=>x.title === this.title)[0];
-        debugger;
         this.galleryImages = this.product.Gallery;
-      })
-    }
+    })
+  }
 }
